90d9f30820ef7b1aff868340c22ee7240637f203
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-21 18:15:04 -0500

    testing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function find(string) {
-  let test = undefined;
-  if (isNaN(string)) {
-    if (string.substring) {
-      test = true;
+const testing = {
+  find: function (string) {
+    let test = undefined;
+    if (isNaN(string)) {
+      if (string.substring) {
+        test = true;
+      }
+      return test;
     }
-    return test;
-  }
-}
+  },
+};
 
 module.exports = find;
