610de45ec478e4b809174289f8b7831d3b3e1f64
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-21 22:57:50 -0500

    testing

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ exports.find = function (str) {
   }
   return fg;
 };
+
+exports.count = function (str) {
+  let yc = undefined;
+  if (isNaN(str)) {
+    if (str.substring) {
+      let dk = "🍪";
+      if (str.includes(dk)) {
+        //yc of cookies
+        let gz = str.length;
+        let i = 0;
+        let n = 0;
+        let x = 0;
+        while (i < gz) {
+          i++;
+          if (str[i] === dk) {
+            n = n + 1;
+          }
+        }
+      } else {
+        yc = 0;
+      }
+    }
+  }
+  return yc;
+};
