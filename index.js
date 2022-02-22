870db024761a203be260d464578c4d7925fa451d
Obed Developer <75869356+xXObedXx@users.noreply.github.com>
2022-02-22 10:59:46 -0500

    Under construction.

--- index.js
+++ index.js
@@ -20,27 +20,29 @@ exports.count = function (str) {
   const emojiRegex = /\p{Emoji}/u;
   let cookieRegex = /🍪/g;
   let yc = undefined;
-  if (isNaN(str)) {
-    if (str.substring) {
-      if (str.match(emojiRegex)) {
-        let dk = "🍪";
-        if (str.includes(dk)) {
-          let gz = str.length;
-          let i = 0;
-          let n = 0;
-          while (i < gz) {
-            s = str.split("");
-            c = [];
-            c[0] = s[i] + s[1];
-            let ib = c[0].codePointAt(0).toString(16);
-            if (ib === "1f36a" || ib === "d83c") {
-              n = n + 1;
+  if (str) {
+    if (isNaN(str)) {
+      if (str.substring) {
+        if (str.match(emojiRegex)) {
+          let dk = "🍪";
+          if (str.includes(dk)) {
+            let gz = str.length;
+            let i = 0;
+            let n = 0;
+            while (i < gz) {
+              s = str.split("");
+              c = [];
+              c[0] = s[i] + s[1];
+              let ib = c[0].codePointAt(0).toString(16);
+              if (ib === "1f36a" || ib === "d83c") {
+                n = n + 1;
+              }
+              i++;
             }
-            i++;
+            yc = n;
+          } else {
+            yc = 0;
           }
-          yc = n;
-        } else {
-          yc = 0;
         }
       }
     }
